22c6699ff33a25727a8a4bf733e95755a29cf2d5
Arlen-LT <arlen-lt@protonmail.com>
2022-11-09 20:53:37 +0800

    fix unexported symbol and unpassed macro definition

--- src/DLNAModule.h
+++ src/DLNAModule.h
@@ -10,8 +10,8 @@
 
 #include "upnp.h"
 
-#if ENABLE_SLOG
-#include "contrib/slog/slog.h"
+#ifdef ENABLE_SLOG
+#include "../contrib/slog/slog.h"
 #define Log(x, y, ...) do {                                 \
     slog_tag("DLNAModule", 3 - x, y"\n", ##__VA_ARGS__);     \
 } while (0)
