f52cdc0466afcf13804f4a78adf5ed5b1a62374d
Arlen-LT <arlen-lt@protonmail.com>
2022-11-11 18:30:51 +0800

    add build_win64.ps1

---
+++ build_win64.ps1
@@ -0,0 +1,10 @@
+$vs2022_version=$(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0,18.0]  -property catalog_productDisplayVersion)
+$vs2022_install=$(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0,18.0] -property installationPath)
+echo "Visual Studio Version: $vs2022_version"
+echo "Visual Studio Install Path: $vs2022_install"
+
+$cmake="C:\tools\cmake-3.24.3-windows-x86_64\bin\cmake.exe"
+& $cmake --version
+$ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
+& $ninja --version
+& $cmake --preset win64 -G"Visual Studio 17 2022"
