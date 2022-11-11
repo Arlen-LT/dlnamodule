fe24f7bbafbadcdf01befdbaa05dfdbdd7c4e4f4
Arlen-LT <arlen-lt@protonmail.com>
2022-11-11 10:40:17 +0000

    Update build_win64.ps1

--- build_win64.ps1
+++ build_win64.ps1
@@ -3,10 +3,10 @@ $vs2022_install=$(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs
 echo "Visual Studio Version: $vs2022_version"
 echo "Visual Studio Install Path: $vs2022_install"
 
-$cmake="C:\tools\cmake-3.24.3-windows-x86_64\bin\cmake.exe"
+$cmake="$WINRUNNER_CMAKE_DIR/bin/cmake.exe"
 & $cmake --version
-$ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
-& $ninja --version
+# $ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
+# & $ninja --version
 & $cmake --preset win64 -G"Visual Studio 17 2022"
 pushd win64/build
 & $cmake --build .
