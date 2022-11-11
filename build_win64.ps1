e8ef99dc5cb3266478497a570d187060a426b15f
Arlen-LT <arlen-lt@protonmail.com>
2022-11-11 18:52:48 +0800

    try fix

--- build_win64.ps1
+++ build_win64.ps1
@@ -1,13 +1,16 @@
+param ($cmake_install)
+echo "CMake Install Path: $cmake_install"
+
 $vs2022_version=$(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0,18.0]  -property catalog_productDisplayVersion)
 $vs2022_install=$(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version [17.0,18.0] -property installationPath)
 echo "Visual Studio Version: $vs2022_version"
 echo "Visual Studio Install Path: $vs2022_install"
 
-# $cmake="$WINRUNNER_CMAKE_DIR/bin/cmake.exe"
+$cmake="$cmake_install/bin/cmake.exe"
 & $cmake --version
 # $ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
 # & $ninja --version
-& $cmake --preset win64 -G"Visual Studio 17 2022"
+& $cmake --preset win64 -DENABLE_SLOG=TRUE -G"Visual Studio 17 2022"
 pushd win64/build
 & $cmake --build .
 & $cmake --install .
