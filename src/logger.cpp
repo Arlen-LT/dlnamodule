b927e4125130cbc216002ebd21bb6bddcb8e724c
Arlen-LT <arlen-lt@protonmail.com>
2022-12-15 14:23:46 +0800

    change logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,43 +1,17 @@
-#if not defined ENABLE_SLOG
-#include <fstream>
-
 #include "logger.h"
-#include "DLNAModule.h"
-#if _WIN64
-#define gettid() GetCurrentThreadId()
-#endif //_WIN64
 
-#define LOG_LINE_SIZE 1024
-#if LOG_LINE_SIZE <= 64
-#error "TOO SMALL BUFFER SIZE MAY CAUSE FATAL ERROR."
-#elif LOG_LINE_SIZE <= 256
-#warning "MUST ENSURE ALL THE BUFFER NOT TO OVERFLOW."
-#endif //LOG_LINE_SIZE 
+#if not ENABLE_SLOG
+namespace logger {
 
+    bool SetLogFile(const char* path)
+    {
+        return std::filesystem::exists(const_cast<std::filesystem::path&>(logFile) = std::filesystem::u8path(path)); //TODO: change all string to u8string.
+    }
 
-const char* LOGTAG[] = { "[I]","[W]","[E]" };
-void Log(LogTag level, const char* format, ...)
-{
-	std::ofstream outLogFileStream{ DLNAModule::GetInstance().logFile, std::ios::out | std::ios::app };
-	if (!outLogFileStream.is_open())
-		return;
+    extern "C" void ExternalLog(LogLevel level, const char* format)
+    {
+        Log(level, format);
+    }
 
-	time_t current;
-	time(&current);
-	struct tm* local = localtime(&current);
-
-	char* logInfo = new char[LOG_LINE_SIZE];
-	int offset = snprintf(logInfo, LOG_LINE_SIZE - 1, "%04d/%02d/%02d %02d:%02d:%02d SkyboxSamba: %s[thread %d] ",
-		local->tm_year + 1900, local->tm_mon + 1, local->tm_mday,
-		local->tm_hour, local->tm_min, local->tm_sec,
-		LOGTAG[(int)level], gettid());
-	va_list args;
-	va_start(args, format);
-	vsnprintf(logInfo + offset, LOG_LINE_SIZE - offset - 1, format, args);
-	va_end(args);
-
-	outLogFileStream << logInfo << "\n";
-	delete[] logInfo;
-	outLogFileStream.close();
-}
-#endif
+}// namespace logger
+#endif // ENABLE_SLOG
--- src/logger.h
+++ src/logger.h
@@ -1,16 +1,119 @@
 #pragma once
 
-enum LogTag {
-	LEVEL_INFO,
-	LEVEL_WARNING,
-	LEVEL_ERROR
-};
+#include <mutex>
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <filesystem>
 
-#if ENABLE_SLOG
-#include "../contrib/slog/slog.h"
-#define Log(x, y, ...) do {                                 \
-    slog_tag("DLNAModule", 3 - x, y"\n", ##__VA_ARGS__);     \
-} while (0)
-#else
-void Log(LogTag level, const char* format, ...);
+#if __cpp_lib_format
+#include <format>
 #endif
+
+#if __cpp_lib_source_location
+#include <source_location>
+#endif
+
+#if _WIN64
+#include <windows.h>
+#define gettid() GetCurrentThreadId()
+#elif __ANDROID__
+#include <android/log.h>
+#include <unistd.h>
+#endif
+
+#if not ENABLE_SLOG
+namespace logger {
+
+    const std::filesystem::path logFile;
+    enum class LogLevel : const char
+    {
+        Trace = 'T',
+        Debug = 'D',
+        Info = 'I',
+        Warning = 'W',
+        Error = 'E',
+        Fatal = 'F'
+    };
+
+    extern "C" bool SetLogFile(const char* path);
+    extern "C" void ExternalLog(LogLevel level, const char* format);
+
+    template <typename... Args>
+    struct Log
+    {
+        Log(LogLevel level, const char* format, Args&&... args
+#if __cpp_lib_source_location
+            , const std::source_location& location = std::source_location::current()
+#endif
+        )
+        {
+            std::ofstream logStream{ logFile, std::ios::out | std::ios::app };
+            std::ostream* os = logStream.is_open() ? &logStream : &std::cout;
+            time_t current = std::time(nullptr);
+            *os << std::left
+                << std::put_time(std::localtime(&current), "%F %T ")
+                << "[" << static_cast<char>(level) << "]"
+                << "[thread " << gettid() << "] "
+#if __cpp_lib_source_location
+                << location.file_name()
+                << "(" << location.line() << "," << location.column() << ","
+                << std::quoted(location.function_name())
+                << "): ";
+#endif
+#if __cpp_lib_format and ENABLE_STD_FORMAT
+            * os << std::vformat(std::string_view{ format }, std::make_format_args(args...)) << "\n";
+#else
+            std::string_view sv{ format };
+            auto it = sv.begin();
+            auto TryInsert = [&sv, &it, &os]<typename T>(size_t n, T obj)
+            {
+                for (; it != sv.end(); ++it)
+                {
+                    if (*it == '%')
+                    {
+                        *os << obj;
+                        ++it;
+                        break;
+                    }
+                    else
+                    {
+                        *os << *it;
+                    }
+                }
+            };
+
+            [&] <std::size_t... N>(std::index_sequence<N...>)
+            {
+                ((TryInsert(N, std::get<N>(std::forward_as_tuple(args...)))), ...);
+            }(std::index_sequence_for<Args...>{});
+
+            *os << "\n";
+        }
+    };
+    template <typename... Args>
+    Log(LogLevel, const char*, Args&&...) -> Log<Args...>;
+#endif
+}
+using namespace logger;
+
+#else // ENABLE_SLOG
+enum LogLevel
+{
+    Trace = 16,
+    Debug = 8,
+    Info = 4,
+    Warning = 2,
+    Error = 1,
+    Fatal = 0
+};
+#include "../contrib/slog/slog.h"
+#define Log(x, y, ...) do {                                  \
+    slog_tag("Log", 3 - x, y"\n", ##__VA_ARGS__);            \
+} while (0)
+#endif // ENABLE_SLOG
+
+#define CHECK_VARIABLE(x, y) do {                             \
+   Log(LogLevel::Debug, #x": " y, x);                         \
+} while (0);
+
