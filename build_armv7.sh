3454066f08cfa0177dd2a022f2f3cdf97dc05f89
Arlen <arlen-lt@protonmail.com>
2023-01-10 11:31:02 +0800

    disable rapidjson examples and tests

---
+++ build_armv7.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+if [ -d ./contrib/rapidjson/thirdparty ]
+then
+    rm -rf ./contrib/rapidjson/thirdparty
+fi
+cmake --preset win64 -DBUILD_TESTING=FALSE -DUPNP_ENABLE_IPV6=FALSE -DRAPIDJSON_BUILD_CXX11=FALSE -DRAPIDJSON_BUILD_CXX17=TRUE -DRAPIDJSON_BUILD_DOC=FALSE -DRAPIDJSON_BUILD_EXAMPLES=FALSE -DRAPIDJSON_BUILD_TESTS=FALSE
+pushd armv7/build
+cmake --build .
+cmake --install .
+popd
--- build_win64.ps1
+++ build_win64.ps1
@@ -10,6 +10,9 @@ $cmake="$cmake_install/bin/cmake.exe"
 & $cmake --version
 # $ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
 # & $ninja --version
+if((Test-Path -Path './contrib/rapidjson/thirdparty')){
+	rm -r -force ./contrib/rapidjson/thirdparty
+}
 & $cmake --preset win64 -DBUILD_TESTING=FALSE -DUPNP_ENABLE_IPV6=FALSE -DRAPIDJSON_BUILD_CXX11=FALSE -DRAPIDJSON_BUILD_CXX17=TRUE -DRAPIDJSON_BUILD_DOC=FALSE -DRAPIDJSON_BUILD_EXAMPLES=FALSE -DRAPIDJSON_BUILD_TESTS=FALSE
 pushd win64/build
 & $cmake --build . --config Release
