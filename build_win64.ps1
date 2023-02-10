86d2debd2292619ff090d4316f86697c8a4466e5
Arlen <arlen-lt@protonmail.com>
2023-02-10 19:23:20 +0800

    move all definitions from script to CMakePresets.json

--- build_win64.ps1
+++ build_win64.ps1
@@ -11,9 +11,9 @@ $cmake="$cmake_install/bin/cmake.exe"
 # $ninja="C:\tools\cmake-3.24.3-windows-x86_64\bin\ninja.exe"
 # & $ninja --version
 if((Test-Path -Path './contrib/rapidjson/thirdparty')){
-	rm -r -force ./contrib/rapidjson/thirdparty
+	#rm -r -force ./contrib/rapidjson/thirdparty
 }
-& $cmake --preset win64 -DBUILD_TESTING=FALSE -DUPNP_ENABLE_IPV6=FALSE -DUPNP_BUILD_SAMPLES=FALSE -DRAPIDJSON_BUILD_CXX11=FALSE -DRAPIDJSON_BUILD_CXX17=TRUE -DRAPIDJSON_BUILD_DOC=FALSE -DRAPIDJSON_BUILD_EXAMPLES=FALSE -DRAPIDJSON_BUILD_TESTS=FALSE
+& $cmake --preset win64
 pushd win64/build
 & $cmake --build . --config Release
 & $cmake --install .
